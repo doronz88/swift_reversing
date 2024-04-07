d6affae3b80278734a735dca22c41d9749e9ca04
doronz88 <doron88@gmail.com>
2024-04-07 18:53:44 +0300

    add details and ida script

--- ida_header.h
+++ ida_header.h
@@ -4,35 +4,23 @@ typedef unsigned long long u64;
 typedef s64 Int;
 typedef u64 Bool;
 
-typedef struct {
-    s64 length; 
-    char *ptr; 
-} String;
+struct Swift::String
+{
+  u64 _countAndFlagsBits;
+  void *_object;
+};
 
-typedef struct {
-    s64 intValue;
-    char *ptr;
+union Swift_ElementAny {
+    Swift::String stringElement;
+};
+
+struct Swift_Any {
+    Swift_ElementAny element;
     u64 unknown;
     s64 type;
-} Any;
+};
 
-typedef struct {
+struct Swift_ArrayAny {
     s64 length;
-    Any *items;
-} ArrayAny;
-
-
-// String.init(_builtinStringLiteral:utf8CodeUnitCount:isASCII:)
-String __usercall __spoils<> String_init__builtinStringLiteral_utf8CodeUnitCount_isASCII__@<X1:X0>(char *_builtinStringLiteral@<X0>, u64 utf8CodeUnitCount@<X1>, u64 isASCII@<X2>);
-
-// _allocateUninitializedArray<A>(_:)
-ArrayAny __usercall __spoils<> _allocateUninitializedArray_A_____@<X1:X0>();
-
-// default argument 1 of print(_:separator:terminator:)
-String __usercall __spoils<> default_argument_1_of_print___separator_terminator__@<X1:X0>();
-
-// default argument 2 of print(_:separator:terminator:)
-String __usercall __spoils<> default_argument_2_of_print___separator_terminator__@<X1:X0>();
-
-// print(_:separator:terminator:)
-void __usercall __spoils<> print___separator_terminator__(ArrayAny *items@<X0>, String separator@<X2:X1>, String terminator@<X4:X3>); 
+    Swift_Any *items;
+};
